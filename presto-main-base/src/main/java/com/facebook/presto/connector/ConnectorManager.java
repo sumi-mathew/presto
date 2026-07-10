sumi-mathew/presto

commit c7a444c62a5c7ea8de898a93a45b2b27bcd2e05c

Add SPI support for TableFunctions
Changes adapted from trino/PR#11336, 12350, 12407, 12476, 12531, 12813
Original commits:
395fd91c6480a993241eeabd9599873d0d05b24b
998315075343beecef962657b8cbf440d53cc13b
712b8e98ff8a726f95295ac539159fc532628273
131dc44af97b31a2fa8115028d98d06671641bfa
0da095e14b0855f89af3c4f254a5a60280fc7170
5310671f80291394b12ba2ea746e4e60051aaff4
18bb60262cb0850cf839c2b20b434344921f5122
4a7d72afb64f93a9748a4c6b4defc2d42bbae000
Author: kasiafi

Modifications were made to adapt to Presto including:
Removal of ConnectorExpression.

Co-authored-by: kasiafi <30203062+kasiafi@users.noreply.github.com>
Co-authored-by: Xin Zhang <desertsxin@gmail.com>

presto-main-base/src/main/java/com/facebook/presto/connector/ConnectorManager.java
@@ -51,6 +51,7 @@ import com.facebook.presto.spi.connector.ConnectorPageSourceProvider;
 import com.facebook.presto.spi.connector.ConnectorPlanOptimizerProvider;
 import com.facebook.presto.spi.connector.ConnectorRecordSetProvider;
 import com.facebook.presto.spi.connector.ConnectorSplitManager;
+import com.facebook.presto.spi.function.table.ConnectorTableFunction;
 import com.facebook.presto.spi.procedure.Procedure;
 import com.facebook.presto.spi.relation.DeterminismEvaluator;
 import com.facebook.presto.spi.relation.DomainTranslator;
@@ -323,6 +324,7 @@ public class ConnectorManager
         metadataManager.getSchemaPropertyManager().addProperties(connectorId, connector.getSchemaProperties());
         metadataManager.getAnalyzePropertyManager().addProperties(connectorId, connector.getAnalyzeProperties());
         metadataManager.getSessionPropertyManager().addConnectorSessionProperties(connectorId, connector.getSessionProperties());
+        metadataManager.getFunctionAndTypeManager().getTableFunctionRegistry().addTableFunctions(connectorId, connector.getTableFunctions());
     }
 
     public synchronized void dropConnection(String catalogName)
@@ -334,6 +336,7 @@ public class ConnectorManager
             removeConnectorInternal(connectorId);
             removeConnectorInternal(createInformationSchemaConnectorId(connectorId));
             removeConnectorInternal(createSystemTablesConnectorId(connectorId));
+            metadataManager.getFunctionAndTypeManager().getTableFunctionRegistry().removeTableFunctions(connectorId);
         });
     }
 
@@ -398,6 +401,7 @@ public class ConnectorManager
         private final Set<Procedure> procedures;
 
         private final Set<Class<?>> functions;
+        private final Set<ConnectorTableFunction> connectorTableFunctions;
         private final ConnectorPageSourceProvider pageSourceProvider;
         private final Optional<ConnectorPageSinkProvider> pageSinkProvider;
         private final Optional<ConnectorIndexProvider> indexProvider;
@@ -426,6 +430,10 @@ public class ConnectorManager
             Set<Procedure> procedures = connector.getProcedures();
             requireNonNull(procedures, "Connector %s returned a null procedures set");
             this.procedures = ImmutableSet.copyOf(procedures);
+
+            Set<ConnectorTableFunction> connectorTableFunctions = connector.getTableFunctions();
+            requireNonNull(connectorTableFunctions, format("Connector '%s' returned a null table functions set", connectorId));
+            this.connectorTableFunctions = ImmutableSet.copyOf(connectorTableFunctions);
 
             ConnectorPageSourceProvider connectorPageSourceProvider = null;
             try {
@@ -614,6 +622,11 @@ public class ConnectorManager
         public Optional<ConnectorCodecProvider> getConnectorCodecProvider()
         {
             return connectorCodecProvider;
+        }
+
+        public Set<ConnectorTableFunction> getTableFunctions()
+        {
+            return connectorTableFunctions;
         }
     }
 }

presto-function-server/src/main/java/com/facebook/presto/server/FunctionServerModule.java
@@ -26,6 +26,7 @@ import com.facebook.presto.common.type.Type;
 import com.facebook.presto.functionNamespace.JsonBasedUdfFunctionMetadata;
 import com.facebook.presto.metadata.FunctionAndTypeManager;
 import com.facebook.presto.metadata.HandleResolver;
+import com.facebook.presto.metadata.TableFunctionRegistry;
 import com.facebook.presto.sql.analyzer.FeaturesConfig;
 import com.facebook.presto.sql.analyzer.FunctionsConfig;
 import com.facebook.presto.transaction.NoOpTransactionManager;
@@ -53,6 +54,7 @@ public class FunctionServerModule
     {
         jaxrsBinder(binder).bind(FunctionResource.class);
         binder.bind(FunctionAndTypeManager.class).in(Scopes.SINGLETON);
+        binder.bind(TableFunctionRegistry.class).in(Scopes.SINGLETON);
         binder.bind(TransactionManager.class).to(NoOpTransactionManager.class).in(Scopes.SINGLETON);
         binder.bind(HandleResolver.class).in(Scopes.SINGLETON);
         install(new InternalCommunicationModule());

presto-main-base/src/main/java/com/facebook/presto/metadata/FunctionAndTypeManager.java
@@ -133,6 +133,7 @@ public class FunctionAndTypeManager
 {
     private static final Pattern DEFAULT_NAMESPACE_PREFIX_PATTERN = Pattern.compile("[a-z]+\\.[a-z]+");
     private final TransactionManager transactionManager;
+    private final TableFunctionRegistry tableFunctionRegistry;
     private final BlockEncodingSerde blockEncodingSerde;
     private final BuiltInTypeAndFunctionNamespaceManager builtInTypeAndFunctionNamespaceManager;
     private final FunctionInvokerProvider functionInvokerProvider;
@@ -156,6 +157,7 @@ public class FunctionAndTypeManager
     @Inject
     public FunctionAndTypeManager(
             TransactionManager transactionManager,
+            TableFunctionRegistry tableFunctionRegistry,
             BlockEncodingSerde blockEncodingSerde,
             FeaturesConfig featuresConfig,
             FunctionsConfig functionsConfig,
@@ -163,6 +165,7 @@ public class FunctionAndTypeManager
             Set<Type> types)
     {
         this.transactionManager = requireNonNull(transactionManager, "transactionManager is null");
+        this.tableFunctionRegistry = requireNonNull(tableFunctionRegistry, "tableFunctionRegistry is null");
         this.blockEncodingSerde = requireNonNull(blockEncodingSerde, "blockEncodingSerde is null");
         this.functionsConfig = requireNonNull(functionsConfig, "functionsConfig is null");
         this.types = requireNonNull(types, "types is null");
@@ -192,6 +195,7 @@ public class FunctionAndTypeManager
     {
         return new FunctionAndTypeManager(
                 createTestTransactionManager(),
+                new TableFunctionRegistry(),
                 new BlockEncodingManager(),
                 new FeaturesConfig(),
                 new FunctionsConfig(),
@@ -419,6 +423,11 @@ public class FunctionAndTypeManager
             name = "sql_function_handle";
         }
         handleResolver.addFunctionNamespace(name, factory.getHandleResolver());
+    }
+
+    public TableFunctionRegistry getTableFunctionRegistry()
+    {
+        return tableFunctionRegistry;
     }
 
     public void loadTypeManager(String typeManagerName)

presto-main-base/src/main/java/com/facebook/presto/metadata/MetadataManager.java
@@ -243,7 +243,7 @@ public class MetadataManager
     {
         BlockEncodingManager blockEncodingManager = new BlockEncodingManager();
         return new MetadataManager(
-                new FunctionAndTypeManager(transactionManager, blockEncodingManager, featuresConfig, functionsConfig, new HandleResolver(), ImmutableSet.of()),
+                new FunctionAndTypeManager(transactionManager, new TableFunctionRegistry(), blockEncodingManager, featuresConfig, functionsConfig, new HandleResolver(), ImmutableSet.of()),
                 blockEncodingManager,
                 createTestingSessionPropertyManager(),
                 new SchemaPropertyManager(),

presto-main-base/src/main/java/com/facebook/presto/metadata/TableFunctionMetadata.java
@@ -1,0 +1,41 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.metadata;
+
+import com.facebook.presto.spi.ConnectorId;
+import com.facebook.presto.spi.function.table.ConnectorTableFunction;
+
+import static java.util.Objects.requireNonNull;
+
+public class TableFunctionMetadata
+{
+    private final ConnectorId connectorId;
+    private final ConnectorTableFunction function;
+
+    public TableFunctionMetadata(ConnectorId connectorId, ConnectorTableFunction function)
+    {
+        this.connectorId = requireNonNull(connectorId, "connectorId is null");
+        this.function = requireNonNull(function, "function is null");
+    }
+
+    public ConnectorId getConnectorId()
+    {
+        return connectorId;
+    }
+
+    public ConnectorTableFunction getFunction()
+    {
+        return function;
+    }
+}

presto-main-base/src/main/java/com/facebook/presto/metadata/TableFunctionRegistry.java
@@ -1,0 +1,156 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.metadata;
+
+import com.facebook.presto.Session;
+import com.facebook.presto.spi.ConnectorId;
+import com.facebook.presto.spi.PrestoException;
+import com.facebook.presto.spi.StandardErrorCode;
+import com.facebook.presto.spi.function.CatalogSchemaFunctionName;
+import com.facebook.presto.spi.function.SchemaFunctionName;
+import com.facebook.presto.spi.function.table.ArgumentSpecification;
+import com.facebook.presto.spi.function.table.ConnectorTableFunction;
+import com.facebook.presto.spi.function.table.TableArgumentSpecification;
+import com.facebook.presto.sql.analyzer.SemanticException;
+import com.facebook.presto.sql.tree.QualifiedName;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.errorprone.annotations.ThreadSafe;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static com.facebook.presto.spi.StandardErrorCode.SESSION_CATALOG_NOT_SET;
+import static com.facebook.presto.spi.function.table.Preconditions.checkArgument;
+import static com.facebook.presto.sql.analyzer.SemanticErrorCode.CATALOG_NOT_SPECIFIED;
+import static com.facebook.presto.sql.analyzer.SemanticErrorCode.SCHEMA_NOT_SPECIFIED;
+import static com.google.common.base.Preconditions.checkState;
+import static java.util.Locale.ENGLISH;
+import static java.util.Objects.requireNonNull;
+
+@ThreadSafe
+public class TableFunctionRegistry
+{
+    // catalog name in the original case; schema and function name in lowercase
+    private final Map<ConnectorId, Map<SchemaFunctionName, TableFunctionMetadata>> tableFunctions = new ConcurrentHashMap<>();
+
+    public void addTableFunctions(ConnectorId catalogName, Collection<ConnectorTableFunction> functions)
+    {
+        requireNonNull(catalogName, "catalogName is null");
+        requireNonNull(functions, "functions is null");
+        checkState(!tableFunctions.containsKey(catalogName), "Table functions already registered for catalog: " + catalogName);
+
+        functions.stream()
+                .forEach(TableFunctionRegistry::validateTableFunction);
+
+        ImmutableMap.Builder<SchemaFunctionName, TableFunctionMetadata> builder = ImmutableMap.builder();
+        for (ConnectorTableFunction function : functions) {
+            builder.put(
+                    new SchemaFunctionName(
+                            function.getSchema().toLowerCase(ENGLISH),
+                            function.getName().toLowerCase(ENGLISH)),
+                    new TableFunctionMetadata(catalogName, function));
+        }
+        tableFunctions.putIfAbsent(catalogName, builder.buildOrThrow());
+    }
+
+    public void removeTableFunctions(ConnectorId catalogName)
+    {
+        tableFunctions.remove(catalogName);
+    }
+
+    public static List<CatalogSchemaFunctionName> toPath(Session session, QualifiedName name)
+    {
+        List<String> parts = name.getParts();
+        if (parts.size() > 3) {
+            throw new PrestoException(StandardErrorCode.FUNCTION_NOT_FOUND, "Invalid function name: " + name);
+        }
+        if (parts.size() == 3) {
+            return ImmutableList.of(new CatalogSchemaFunctionName(parts.get(0), parts.get(1), parts.get(2)));
+        }
+
+        if (parts.size() == 2) {
+            String currentCatalog = session.getCatalog()
+                    .orElseThrow(() -> new PrestoException(SESSION_CATALOG_NOT_SET, "Session default catalog must be set to resolve a partial function name: " + name));
+            return ImmutableList.of(new CatalogSchemaFunctionName(currentCatalog, parts.get(0), parts.get(1)));
+        }
+
+        ImmutableList.Builder<CatalogSchemaFunctionName> names = ImmutableList.builder();
+
+        String currentCatalog = session.getCatalog()
+                .orElseThrow(() -> new SemanticException(CATALOG_NOT_SPECIFIED, "Catalog must be specified when session catalog is not set"));
+        String currentSchema = session.getSchema()
+                .orElseThrow(() -> new SemanticException(SCHEMA_NOT_SPECIFIED, "Schema must be specified when session schema is not set"));
+
+        // add resolved path items
+        names.add(new CatalogSchemaFunctionName(currentCatalog, currentSchema, parts.get(0)));
+        return names.build();
+    }
+
+    /**
+     * Resolve table function with given qualified name.
+     * Table functions are resolved case-insensitive for consistency with existing scalar function resolution.
+     */
+    public Optional<TableFunctionMetadata> resolve(Session session, QualifiedName qualifiedName)
+    {
+        for (CatalogSchemaFunctionName name : toPath(session, qualifiedName)) {
+            ConnectorId connectorId = new ConnectorId(name.getCatalogName());
+            Map<SchemaFunctionName, TableFunctionMetadata> catalogFunctions = tableFunctions.get(connectorId);
+            if (catalogFunctions != null) {
+                String lowercasedSchemaName = name.getSchemaFunctionName().getSchemaName().toLowerCase(ENGLISH);
+                String lowercasedFunctionName = name.getSchemaFunctionName().getFunctionName().toLowerCase(ENGLISH);
+                TableFunctionMetadata function = catalogFunctions.get(new SchemaFunctionName(lowercasedSchemaName, lowercasedFunctionName));
+                if (function != null) {
+                    return Optional.of(function);
+                }
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    private static void validateTableFunction(ConnectorTableFunction tableFunction)
+    {
+        requireNonNull(tableFunction, "tableFunction is null");
+        requireNonNull(tableFunction.getName(), "table function name is null");
+        requireNonNull(tableFunction.getSchema(), "table function schema name is null");
+        requireNonNull(tableFunction.getArguments(), "table function arguments is null");
+        requireNonNull(tableFunction.getReturnTypeSpecification(), "table function returnTypeSpecification is null");
+
+        checkArgument(!tableFunction.getName().isEmpty(), "table function name is empty");
+        checkArgument(!tableFunction.getSchema().isEmpty(), "table function schema name is empty");
+
+        Set<String> argumentNames = new HashSet<>();
+        int tableArgumentsWithRowSemantics = 0;
+        for (ArgumentSpecification specification : tableFunction.getArguments()) {
+            if (!argumentNames.add(specification.getName())) {
+                throw new IllegalArgumentException("duplicate argument name: " + specification.getName());
+            }
+
+            if (specification instanceof TableArgumentSpecification &&
+                    ((TableArgumentSpecification) specification).isRowSemantics()) {
+                tableArgumentsWithRowSemantics++;
+            }
+        }
+        checkArgument(tableArgumentsWithRowSemantics <= 1, "more than one table argument with row semantics");
+        // The 'keep when empty' or 'prune when empty' property must not be explicitly specified for a table argument with row semantics.
+        // Such a table argument is implicitly 'prune when empty'. The TableArgumentSpecification.Builder enforces the 'prune when empty' property
+        // for a table argument with row semantics.
+    }
+}

presto-main-base/src/main/java/com/facebook/presto/testing/LocalQueryRunner.java
@@ -109,6 +109,7 @@ import com.facebook.presto.metadata.MetadataUtil;
 import com.facebook.presto.metadata.QualifiedTablePrefix;
 import com.facebook.presto.metadata.SchemaPropertyManager;
 import com.facebook.presto.metadata.Split;
+import com.facebook.presto.metadata.TableFunctionRegistry;
 import com.facebook.presto.metadata.TablePropertyManager;
 import com.facebook.presto.nodeManager.PluginNodeManager;
 import com.facebook.presto.operator.Driver;
@@ -433,7 +434,7 @@ public class LocalQueryRunner
         featuresConfig.setIgnoreStatsCalculatorFailures(false);
 
         this.metadata = new MetadataManager(
-                new FunctionAndTypeManager(transactionManager, blockEncodingManager, featuresConfig, functionsConfig, new HandleResolver(), ImmutableSet.of()),
+                new FunctionAndTypeManager(transactionManager, new TableFunctionRegistry(), blockEncodingManager, featuresConfig, functionsConfig, new HandleResolver(), ImmutableSet.of()),
                 blockEncodingManager,
                 createTestingSessionPropertyManager(
                         new SystemSessionProperties(

commit 2cbac501f8dafffcf6fac4dd112b98bf3594d4ab

fix: Address catalog update failure due to incomplete TVF cleanup on catalog drop (#28112)
## Description
When a catalog is created in Presto, three internal connectors are
registered under the hood:

| Connector ID | Role |
|---|---|
| `tpch_1` | Main connector |
| `$info_schema@tpch_1` | Information schema connector |
| `$system@tpch_1` | System tables connector |

`ConnectorManager.addConnectorInternal()` unconditionally calls both of
the following
for **every one of the three connectors**:

```java
getTableFunctionRegistry().addTableFunctions(connectorId, ...);          // TableFunctionRegistry
getFunctionAndTypeManager().addTableFunctionProcessorProvider(connectorId, ...); // ConcurrentHashMap
```

However, the cleanup in `dropConnection()` and the rollback handler in
`addCatalogConnector()` only removed entries for the **main connector**,
leaving stale entries behind for `$info_schema@tpch_1` and
`$system@tpch_1`.

On the next CREATE or UPDATE for the same catalog name, both
registration
methods throw on the stale entries:

- `TableFunctionRegistry.addTableFunctions()` — strict
`checkState(!containsKey(...))`
- `FunctionAndTypeManager.addTableFunctionProcessorProvider()` — strict
`putIfAbsent` + throw

This results in an HTTP 500 error for any catalog update (DELETE then
re-CREATE, or PUT).

## Motivation and Context

This bug was introduced in commit `c7a444c62a` ("Add SPI support for
TableFunctions")
which added `addTableFunctions` / `removeTableFunctions` but only
cleaned up the main
connector in `dropConnection`.

It was further compounded by commit `c2b0f5f186` ("feat: TVF Part 7/X
Final PR of
adaptation changes #26445") which added
`addTableFunctionProcessorProvider` /
`removeTableFunctionProcessorProvider` following the same incomplete
cleanup pattern.

The bug affects any Presto deployment that:
1. Uses the catalog management REST API (`PUT /v1/catalog/{name}` or
   `DELETE` followed by `POST /v1/catalog/{name}`)

presto-main-base/src/main/java/com/facebook/presto/connector/ConnectorManager.java
@@ -356,13 +356,13 @@ public class ConnectorManager
             removeConnectorInternal(connectorId);
             removeConnectorInternal(createInformationSchemaConnectorId(connectorId));
             removeConnectorInternal(createSystemTablesConnectorId(connectorId));
-            metadataManager.getFunctionAndTypeManager().getTableFunctionRegistry().removeTableFunctions(connectorId);
-            metadataManager.getFunctionAndTypeManager().removeTableFunctionProcessorProvider(connectorId);
         });
     }
 
     private synchronized void removeConnectorInternal(ConnectorId connectorId)
     {
+        metadataManager.getFunctionAndTypeManager().getTableFunctionRegistry().removeTableFunctions(connectorId);
+        metadataManager.getFunctionAndTypeManager().removeTableFunctionProcessorProvider(connectorId);
         splitManager.removeConnectorSplitManager(connectorId);
         pageSourceManager.removeConnectorPageSourceProvider(connectorId);
         pageSinkManager.removeConnectorPageSinkProvider(connectorId);